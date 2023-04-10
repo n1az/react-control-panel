51d35b77371e176fbdc7ece6c6cef01fe352a847
n1az <muhammadniazmorshed@gmail.com>
2023-04-10 18:18:32 +0200

    completed scenes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline,ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar"
+import Dashboard from "./scenes/dashboard";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -11,6 +13,9 @@ function App() {
         <div className="app">
           <main classname="content">
             <Topbar />  
+            <Routes>
+              <Route path="/" element={<Dashboard/>} />
+            </Routes>
           </main>  
         </div>;
       </ThemeProvider>
