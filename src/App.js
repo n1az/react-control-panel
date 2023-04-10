2404c32c1d60c61b27cf838b26f670fc26517494
n1az <muhammadniazmorshed@gmail.com>
2023-04-11 00:35:34 +0200

    sidebar and topbar completed

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
-import Dashboard from "./scenes/dashboard";
-import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
-import Bar from "./scenes/bar";
-import Form from "./scenes/form";
-import Line from "./scenes/line";
-import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
+import Dashboard from "./scenes/dashboard/index";
+import Team from "./scenes/team/index";
+import Invoices from "./scenes/invoices/index";
+import Contacts from "./scenes/contacts/index";
+import Bar from "./scenes/bar/index";
+import Form from "./scenes/form/index";
+import Line from "./scenes/line/index";
+import Pie from "./scenes/pie/index";
+import FAQ from "./scenes/faq/index";
+import Geography from "./scenes/geography/index";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Calendar from "./scenes/calendar/calendar";
@@ -29,7 +29,7 @@ function App() {
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
-              <Route path="/" element={<Dashboard />} />
+              <Route path="/React" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
